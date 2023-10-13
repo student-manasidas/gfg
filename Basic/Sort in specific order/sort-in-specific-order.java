f114541ad1f456ac08f7e3265ab99f4e68ab87e4
student-manasidas <147082021+student-manasidas@users.noreply.github.com>
2023-10-13 08:58:06 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sort in specific order/sort-in-specific-order.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution ob = new Solution();
+            ob.sortIt(a, n);
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+            output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    public void sortIt(long arr[], long n)
+    {
+        //code here
+        ArrayList<Long>even=new ArrayList<>();
+        ArrayList<Long>odd=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                even.add(arr[i]);
+            }
+            else{
+                odd.add(arr[i]);
+            }
+        }
+        Collections.sort(even);
+        Collections.sort(odd,Collections.reverseOrder());
+        int i=0;
+        for(int j=0;j<odd.size();j++){
+            arr[i]=odd.get(j);
+            i++;
+        }
+        for(int j=0;j<even.size();j++){
+            arr[i]=even.get(j);
+            i++;
+        }
+    }
+}
