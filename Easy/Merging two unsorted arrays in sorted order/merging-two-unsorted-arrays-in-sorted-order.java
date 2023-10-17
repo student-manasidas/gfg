495ba75b22c07e429d07162063aac4de3e7aed1a
student-manasidas <147082021+student-manasidas@users.noreply.github.com>
2023-10-17 23:20:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Merging two unsorted arrays in sorted order/merging-two-unsorted-arrays-in-sorted-order.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    void sortedMerge(long A[], long B[], long res[], int N,int M)
+    {
+        // Your code goes here
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int i=0;
+        int j=0;
+        // Make a new array
+        int k=0;
+        while(i<N && j<M){
+            if(A[i]<=B[j]){
+                res[k++]=A[i];
+                i++;
+            }
+            else{
+                res[k++]=B[j];
+                j++;
+            }
+        }
+        while(i<N){
+            res[k++]=A[i];
+            i++;
+        }
+        while(j<M){
+            res[k++]=B[j];
+            j++;
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int m =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+            String line2 = br.readLine();
+            String[] a2 = line2.trim().split("\\s+");
+            long b[] = new long[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Long.parseLong(a2[i]);
+            }
+            long res[] = new long[500001];
+            Solution ob = new Solution();
+            ob.sortedMerge(a,b,res,n,m);
+            for (int i = 0; i < n+m; i++)
+                System.out.print(res[i]+" ");
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
