96a1e36f7be0807d6b7a82bf451e3c7e83d2a3a5
student-manasidas <147082021+student-manasidas@users.noreply.github.com>
2023-10-20 00:25:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class count_pairs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n1 = sc.nextInt();
+            Integer arr1[] = new Integer[n1];
+
+            for (int i = 0; i < n1; i++) arr1[i] = sc.nextInt();
+
+            int n2 = sc.nextInt();
+            Integer arr2[] = new Integer[n2];
+
+            for (int i = 0; i < n2; i++) arr2[i] = sc.nextInt();
+
+            LinkedList<Integer> head1 = new LinkedList<>(Arrays.asList(arr1));
+            LinkedList<Integer> head2 = new LinkedList<>(Arrays.asList(arr2));
+
+            int x = sc.nextInt();
+            Solution gfg = new Solution();
+            System.out.println(gfg.countPairs(head1, head2, x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// your task is to complete this function
+
+/*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
+                          int x) {
+        // Using set 
+        int count=0;
+        Set<Integer>set2=new HashSet<>(head2);
+        for(int num:head1){
+            int collect=x-num;
+            if(set2.contains(collect)){
+                count++;
+            }
+        }
+        return count;
+        
+        
+        
+    }
+}
