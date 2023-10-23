34af1b340d3b58ad5e073e67e0b75631b06183d3
student-manasidas <147082021+student-manasidas@users.noreply.github.com>
2023-10-23 15:36:22 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Exceptionally odd/exceptionally-odd.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().getOddOccurrence(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getOddOccurrence(int[] arr, int n) {
+        // code here
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],1);
+            }
+            else{
+                int oldval=map.getOrDefault(arr[i],0);
+                map.put(arr[i],oldval+1);
+            }
+        }
+        for(Integer k:map.keySet()){
+            if(map.get(k)%2!=0){
+                return k;
+            }
+        }
+        return -1;
+    }
+}
